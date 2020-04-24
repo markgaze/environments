43b65b83c9d35d062ee49da497cc58d37ea28d12
markgaze <mark.l.gaze@gmail.com>
2020-04-24 23:30:24 +0100

    Remove apps that I don't need

--- windows/10/scripts/boxstarter.ps1
+++ windows/10/scripts/boxstarter.ps1
@@ -29,28 +29,28 @@ Add-AppxPackage -Path ~/Ubuntu.appx
 dism /online /NoRestart /Enable-Feature /FeatureName:NetFx3
 
 #--- Tools ---
-choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"' -y
-choco install sysinternals -y
-choco install nodejs -y
+# choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"' -y
+# choco install sysinternals -y
+# choco install nodejs -y
  
 #--- Apps ---
-choco install 7zip -y
-choco install googlechrome -y
-choco install docker-for-windows -y
-choco install sharex -y
 choco install vscode -y
-choco install hyper -y
-choco install paint.net -y
-choco install mremoteng -y
-choco install notepadplusplus -y
-choco install microsoft-teams -y
-choco install ilspy -y
-choco install sql-operations-studio -y
+choco install googlechrome -y
+# choco install 7zip -y
+# choco install docker-for-windows -y
+# choco install sharex -y
+# choco install hyper -y
+# choco install paint.net -y
+# choco install mremoteng -y
+# choco install notepadplusplus -y
+# choco install microsoft-teams -y
+# choco install ilspy -y
+# choco install sql-operations-studio -y
 
 #--- Visual Studio ---
-choco install visualstudio2017enterprise -y
-choco install visualstudio2017-workload-manageddesktop -y
-choco install visualstudio2017-workload-netcoretools -y
+# choco install visualstudio2017enterprise -y
+# choco install visualstudio2017-workload-manageddesktop -y
+# choco install visualstudio2017-workload-netcoretools -y
 
 #--- VS Code ---
 code --install-extension Shan.code-settings-sync
