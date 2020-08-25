2616891edbbeea1063344bf2501fa46c73a65baf
markgaze <mark.l.gaze@gmail.com>
2020-08-25 10:41:12 +0100

    Add nvm profile to .bash_profile instead of .bashrc

--- software/node/install.sh
+++ software/node/install.sh
@@ -2,7 +2,8 @@
 
 echo "Installing nodejs..."
 install-package curl
+export PROFILE=~/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 # shellcheck disable=SC1090
-source ~/.bashrc
+source ~/.bash_profile
 nvm install node
